a1c31af7d52617ffa77c3ebbdba71cf9374d0479
Keisuki <willofthestars@gmail.com>
2017-11-11 13:15:31 +0000

    Fixed Interface Scaling and more.
    - Moved interface scripts into their own folder.
    - Created a dialog to confirm returning to menu.
    - Improved UI scaling with different monitor sizes.
    - Changed coefficient for camera panning to make it easier to control.

---
+++ Assets/Scripts/Interface/InterfaceResize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceResize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 bottomleft = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 10));
+		Vector3 topright = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, 10));
+		float xwidth = Mathf.Abs (topright.x - bottomleft.x);
+		float ywidth = Mathf.Abs (topright.y - bottomleft.y);
+		float newscale = xwidth / 6.66f;
+		transform.Find ("Interface").localScale = new Vector3 (newscale, newscale, 1);
+		transform.Find ("MenuButtonInterface").localPosition = new Vector3 (xwidth / 2.0f, ywidth / 2.0f, 9);
+	}
+}
---
+++ Assets/Scripts/Interface/ShowHideButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideButton : Tappable {
+	public GameObject toShowHide;
+	public bool newActiveState;
+	// Use this for initialization
+	public override void OnTap ()
+	{
+		toShowHide.SetActive (newActiveState);
+	}
+}
