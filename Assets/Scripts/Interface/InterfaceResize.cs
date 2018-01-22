de3e03bc210f34d387fad9d18ee5431e72a88e3f
Keisuki <willofthestars@gmail.com>
2018-01-22 22:56:51 +0000

    Changed Interface for Landscape Mode Play
    When in landscape mode, the main interface will now be half the usual size, and stick to the bottom-left corner, so as not to obscure most of the board.  This triggers when the screen is wider than it is tall.

--- Assets/Scripts/Interface/InterfaceResize.cs
+++ Assets/Scripts/Interface/InterfaceResize.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InterfaceResize : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
@@ -13,13 +12,20 @@ public class InterfaceResize : MonoBehaviour {
 	void OnPreRender () {
 		Vector3 bottomleft = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 10));
 		Vector3 topright = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, 10));
-
 		float xwidth = Mathf.Abs (topright.x - bottomleft.x);
 		float ywidth = Mathf.Abs (topright.y - bottomleft.y);
-		float newscale = xwidth / 6.66f;
-		transform.Find ("Interface").localScale = new Vector3 (newscale, newscale, 1);
+		if (xwidth > ywidth) {
+			float newscale = xwidth / 13.32f;
+			transform.Find ("Interface").localScale = new Vector3 (newscale, newscale, 1);
+			transform.Find ("Interface").localPosition = new Vector3 (bottomleft.x + newscale * 3.34f - transform.position.x, bottomleft.y - transform.position.y  + newscale*1.12f, 9);
+		} else {
+			float newscale = xwidth / 6.66f;
+			transform.Find ("Interface").localScale = new Vector3 (newscale, newscale, 1);
+			transform.Find ("Interface").localPosition = new Vector3 (0, bottomleft.y - transform.position.y  + newscale*1.12f, 9);
+		}
+
 		transform.Find ("MenuButtonInterface").localPosition = new Vector3 (xwidth / 2.0f, ywidth / 2.0f, 9);
 		transform.Find ("Counters").localPosition = new Vector3 (-xwidth / 2.0f, ywidth / 2.0f, 9);
-		transform.Find ("Interface").localPosition = new Vector3 (0, bottomleft.y - transform.position.y  + newscale*1.12f, 9);
+
 	}
 }
